f4fb4da3764c7d393ca0128f66c85f3b8ce58e11
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-03-28 18:05:27 +0200

    added searchbutton with styling

--- frontend/src/Components/Searchbar.js
+++ frontend/src/Components/Searchbar.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const Searchbar = ({ handleSearch }) => {
-  const handleSearchWord = (word) => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchedTerm = (word) => {
     handleSearch(word);
   };
 
   return (
-    <SearchInput
-      type='text'
-      placeholder='Search...'
-      name='search'
-      onChange={event => {
-        handleSearchWord(event.target.value)
-      }}
-    />
+    <div>
+      <SearchInput
+        type='text'
+        placeholder='Search...'
+        name='search'
+        onChange={event => {
+          setSearchTerm(event.target.value)
+        }}
+      />
+      <SearchButton onClick={() => handleSearchedTerm(searchTerm)}>Search</SearchButton>
+    </div>
   )
 }
 
@@ -25,7 +31,7 @@ const SearchInput = styled.input`
   height: 40px;
   font-size: 16px;
   padding-left: 10px;
-  border-radius: 4px;
+  border-radius: 4px 0 0 4px;
   border: 1px #ccc6c6 solid;
   outline: none;
   font-family: Arial, Helvetica, sans-serif;
@@ -40,5 +46,19 @@ const SearchInput = styled.input`
   }
   @media (min-width: 1024px) {
     width: 20%;
+  }
+`;
+
+const SearchButton = styled.button`
+  height: 44px;
+  font-size: 16px;
+  cursor: pointer;
+  background: rgb(198,226,255);
+  border: none;
+  border-radius: 0 4px 4px 0;
+  outline: none;
+
+  &:hover {
+    background: rgb(208,236,265);
   }
 `;
