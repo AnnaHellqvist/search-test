6b293a28814cb5601103d70cad1ddcb9fac135a0
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-03-28 13:59:50 +0200

    added URL and state for imageList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 import Searchbar from './Components/Searchbar';
 
 const App = () => {
+  const [imageList, setImageList] = useState([]);
+
+  const API_URL = `http://localhost:3000/api?s=${searchTerm}`;
+  
   return (
     <div className="App">
       <Searchbar />
