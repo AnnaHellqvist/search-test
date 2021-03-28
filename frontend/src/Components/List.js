4903f87a8f11dedcc7fc8895d2bf89d04fe89e74
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-03-28 15:24:59 +0200

    added styling List component

--- frontend/src/Components/List.js
+++ frontend/src/Components/List.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const List = ({images}) => {
   return (
-    <section>
+    <ImageContainer>
       {images.map(image => {
         return (
-          <img src={image.url} />
+          <StyledImage key={image.url} src={image.url} />
         )
       })}
-    </section>
+    </ImageContainer>
   )
 };
 
 export default List;
+
+const StyledImage = styled.img`
+  width: 80%;
+  margin: 10px 0;
+
+  @media (min-width: 667px) {
+    width: auto;
+    height: 100px;
+    margin: 10px 5px;
+  }
+  @media (min-width: 1024px) {
+    height: 150px;
+  }
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  box-sizing: border-box;
+
+  @media (min-width: 667px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 0 20px;
+    justify-content: space-between;
+  }
+`;
